e1cae94f3de455887cecb881088ffa28f4b853ba
kedweber <keddrin@hotmail.com>
2017-02-02 13:33:39 +0100

    Panic Ugggh! PHPStorm is making me love VIM

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -2,13 +2,15 @@
 /**
  * Global Configuration Override
  *
+ *
  * You can use this file for overriding configuration values from modules, etc.
  * You would place values in here that are agnostic to the environment and not
  * sensitive to security.
  *
  * @NOTE: In practice, this file will typically be INCLUDED in your source
  * control, so do not include passwords or other sensitive information in this
- * file.
+ * file. Your private settings should be in the project/config/autoload/ folder
+ * as local.php or with a nomenclature similar to something.local.php
  */
 //config/autoload/doctrine.global.php
 return array(
